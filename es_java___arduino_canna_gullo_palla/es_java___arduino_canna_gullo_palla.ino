232f00a5230a9a2c2a890e7867a137bcb9427940
Antonio Pellegrino <Pellegrino_Antonio@JeanMonnet.local>
2017-02-13 13:32:01 +0100

    prima versione del programma con arduino

---
+++ es_java___arduino_canna_gullo_palla/es_java___arduino_canna_gullo_palla.ino
@@ -0,0 +1,15 @@
+#include <LiquidCrystal.h>
+#include <dht.h>
+dht DHT;
+#define DHT11_PIN 1
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly
+  Serial.println(DHT.humidity);
+  Serial.println(DHT.temperature);
+  delay(500);
+}
